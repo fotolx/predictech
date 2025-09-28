375264991f5dd59aad3e14069686bdaed6972827
Denis Zhuravlev <87717018+Git-Denshik@users.noreply.github.com>
2025-09-28 10:03:46 +0300

    Update loadComponents.js

--- predictech/static/js/loadComponents.js
+++ predictech/static/js/loadComponents.js
@@ -1,5 +1,4 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Функция для загрузки и вставки HTML-файлов
     function loadHTMLFile(url, elementId) {
         return fetch(url)
             .then(response => {
@@ -32,10 +31,10 @@ document.addEventListener("DOMContentLoaded", function () {
         { url: "public/components/side-panel.html", id: "side-panel" },
         { url: "public/components/modal-confirm.html", id: "modal-confirm" },
         { url: "public/components/modal-risk.html", id: "modal-risk" },
-        { url: "public/components/modal-card-event.html", id: "modal-card-event" }
+        { url: "public/components/modal-card-event.html", id: "modal-card-event" },
+        { url: "public/components/svg-map.html", id: "svg-container" },
+        { url: "public/components/main-svg-map.html", id: "main-svg-container" }
     ];
-
-    // Создаем массив промисов для всех загрузок
     const loadPromises = filesToLoad.map(file => 
         loadHTMLFile(file.url, file.id)
     );
@@ -47,4 +46,5 @@ document.addEventListener("DOMContentLoaded", function () {
             console.error("Ошибка при загрузке компонентов:", error);
         });
 });
+
 
