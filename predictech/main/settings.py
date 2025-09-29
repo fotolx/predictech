c6a5df5471baec32a4a75e132160f380b6c52a70
fotolx <63285940+fotolx@users.noreply.github.com>
2025-09-29 15:48:04 +0300

    Moving static files to correct place

--- predictech/main/settings.py
+++ predictech/main/settings.py
@@ -130,6 +130,7 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
