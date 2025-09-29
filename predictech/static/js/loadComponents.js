705772080215e0527315b35c49c7db65eec4a6a3
Denis Zhuravlev <87717018+Git-Denshik@users.noreply.github.com>
2025-09-29 23:44:17 +0300

    Update loadComponents.js

--- predictech/static/js/loadComponents.js
+++ predictech/static/js/loadComponents.js
@@ -24,17 +24,17 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Массив файлов для загрузки с соответствующими ID
     const filesToLoad = [
-        { url: "static/public/components/header.html", id: "header-container" },
-        { url: "static/public/components/header-navigation.html", id: "header-navigation" },
-        { url: "static/public/components/actions.html", id: "actions" },
-        { url: "static/public/components/indicator.html", id: "state-indicator" },
-        { url: "static/public/components/side-panel.html", id: "side-panel" },
-        { url: "static/public/components/modal-confirm.html", id: "modal-confirm" },
-        { url: "static/public/components/modal-risk.html", id: "modal-risk" },
-        { url: "static/public/components/modal-card-event.html", id: "modal-card-event" },
-        { url: "static/public/components/svg-map.html", id: "svg-container" },
-        { url: "static/public/components/main-svg-map.html", id: "main-svg-container" },
-        { url: "static/public/components/modal-card-event-2.html", id: "modal-card-event-2" }
+        { url: "/static/public/components/header.html", id: "header-container" },
+        { url: "/static/public/components/header-navigation.html", id: "header-navigation" },
+        { url: "/static/public/components/actions.html", id: "actions" },
+        { url: "/static/public/components/indicator.html", id: "state-indicator" },
+        { url: "/static/public/components/side-panel.html", id: "side-panel" },
+        { url: "/static/public/components/modal-confirm.html", id: "modal-confirm" },
+        { url: "/static/public/components/modal-risk.html", id: "modal-risk" },
+        { url: "/static/public/components/modal-card-event.html", id: "modal-card-event" },
+        { url: "/static/public/components/svg-map.html", id: "svg-container" },
+        { url: "/static/public/components/main-svg-map.html", id: "main-svg-container" },
+        { url: "/static/public/components/modal-card-event-2.html", id: "modal-card-event-2" }
     ];
 
     // Создаем массив промисов для всех загрузок
@@ -49,6 +49,7 @@ document.addEventListener("DOMContentLoaded", function () {
             console.error("Ошибка при загрузке компонентов:", error);
         });
 });
+
 
 
 
